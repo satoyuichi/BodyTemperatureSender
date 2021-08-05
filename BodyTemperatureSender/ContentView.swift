7639a895f97f5a24492583c5f01e33272ec39966
Yuichi Sato <satoyuichi80@gmail.com>
2021-08-05 23:00:00 +0900

    Add interface

--- BodyTemperatureSender/ContentView.swift
+++ BodyTemperatureSender/ContentView.swift
@@ -9,13 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            HStack {
+                Text("集計単位")
+                Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
+                    Text("日").tag(1)
+                    Text("週").tag(2)
+                    Text("月").tag(3)
+                }
+            }
+
+            HStack {
+                Text("ピポッド")
+                Picker(selection: /*@START_MENU_TOKEN@*/.constant(1)/*@END_MENU_TOKEN@*/, label: /*@START_MENU_TOKEN@*/Text("Picker")/*@END_MENU_TOKEN@*/) {
+                    Text("最高").tag(1)
+                    Text("平均").tag(2)
+                    Text("最低").tag(3)
+                }
+            }
+            
+            GroupBox(label: Text("出力")) {
+                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                    Text("年月日")
+                }
+            }
+            
+            Button(action: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/{}/*@END_MENU_TOKEN@*/) {
+                Text("送信")
+            }
+        }
+
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
     }
 }
