7977b6d6120c4e1ff5e30fd432d6b665a3cab35d
Yuichi Sato <satoyuichi80@gmail.com>
2021-08-05 21:57:35 +0900

    Initial Commit

---
+++ BodyTemperatureSender/BodyTemperatureSenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BodyTemperatureSenderApp.swift
+//  BodyTemperatureSender
+//
+//  Created by 佐藤雄一 on 2021/08/05.
+//
+
+import SwiftUI
+
+@main
+struct BodyTemperatureSenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BodyTemperatureSender/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BodyTemperatureSender
+//
+//  Created by 佐藤雄一 on 2021/08/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
